39e0146113f7714c66feb5e7eb519556ebe468d5
SivaBalajiEevana-12 <eevanasivabalaji@gmail.com>
2025-04-08 22:56:27 +0530

    routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from 'hono'
+import indes from './routes/indes'
 
 const app = new Hono()
-
+app.route('/',indes)
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
---
+++ backend/src/routes/indes.ts
@@ -0,0 +1,9 @@
+import { Hono } from 'hono'
+
+const indes = new Hono()
+
+indes.get('/user', (c) => {
+  return c.text('Hello Hono from routes folder!')
+})
+
+export default indes
